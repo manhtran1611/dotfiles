304e6d6521b250d615b6ae9049800747e00ca7d7
Tran Duc Manh <trandmanh1999@gmail.com>
2024-01-01 17:30:59 +0700

    feat: update config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,7 @@ alias ls "ls -p -G"
 alias la "ls -A"
 alias ll "ls -l" 
 alias lla "ll -A"
+alias gg "ghq get"
 alias g git
 alias v nvim
 command -qv nvim && alias vim nvim
@@ -17,6 +18,7 @@ set -gx EDITOR nvim
 set -gx PATH bin $PATH
 set -gx PATH ~/bin $PATH
 set -gx PATH ~/.local/bin $PATH
+set -gx PATH ~/.deno/bin $PATH
 
 # NodeJS
 set -gx PATH node_modules/.bin $PATH
@@ -37,4 +39,12 @@ end
 set LOCAL_CONFIG (dirname (status --current-filename))/config-local.fish
 if test -f $LOCAL_CONFIG
   source $LOCAL_CONFIG
+end
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+if type -q afx
+  afx completion fish | source
 end
--- .config/fish/functions/peco_change_directory.fish
+++ .config/fish/functions/peco_change_directory.fish
@@ -17,7 +17,7 @@ function peco_change_directory
   begin
     echo $HOME/.config
     ghq list -p
-    ls -ad */|perl -pe "s#^#$PWD/#"|grep -v \.git
+    ls -ad *|perl -pe "s#^#$PWD/#"|grep -v \.git
     ls -ad $HOME/Developments/*/* |grep -v \.git
   end | sed -e 's/\/$//' | awk '!a[$0]++' | _peco_change_directory $argv
 end
